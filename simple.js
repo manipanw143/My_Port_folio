a4579685edc4b124d3f5264ea06fde4060990ae4
manipanw143 <mp756045@gmail.com>
2024-11-17 18:02:33 +0530

    chan

---
+++ simple.js
@@ -0,0 +1,191 @@
+
+// function my (){
+    
+//     var b = 10;
+//     console.log(b);
+
+// }
+// my()
+// console.log(b);
+
+
+
+
+// function my (){
+    
+//     var b = 10;
+//     console.log(b);
+
+// }
+
+
+// function second () {
+//     my()
+// }
+
+// second()
+
+
+
+// console.log(a);
+
+// let a;
+// var b = 100;
+
+
+// const x = 10;
+// {
+//     const x = 100;
+//     console.log(x)
+// }console.log(x);
+
+
+// var y = 10;
+// {
+//     var y = 100;
+//     console.log(y);
+// }
+// console.log(y)
+
+
+// var y = 10;
+// {
+//     let y = 100;
+//     console.log(y);
+// }
+// console.log(y)
+
+
+// let y = 10;
+// {
+//     var y = 100;
+//     console.log(y);
+// }
+// console.log(y)
+
+
+
+
+// var a = 10;
+// var a = 100;
+// console.log(a);
+
+
+// let a = 10;
+// let a = 100;
+// console.log(a);
+
+// var b = 1;
+// var b =2;
+// console.log(b)
+
+
+
+
+// function x(){
+//    var a = 10;
+//     // console.log(a)
+//     y()
+//     function y(){
+//         console.log(a);
+//     }
+// }
+
+// x();
+
+
+
+
+// function example(funct){
+//     console.log("example")
+//     funct();
+// }
+// example(anotherfunc);
+
+
+// function anotherfunc(){
+//     console.log("another funct");
+// }
+
+
+
+
+// console.log("start");
+
+
+// function cb () {
+//     console.log("call back")
+// }
+
+// setTimeout(cb, 0);
+
+// console.log("end")
+
+
+
+
+
+// function x() {
+
+// }
+
+
+// function y(x){
+// return x;
+// }
+
+
+
+
+
+
+// const arr = [5,1,3,2,6];
+
+// const output = arr.reduce(function (accu, curr){
+//    if(curr>accu){
+//     accu = curr;
+//    }
+//    return accu;
+// },0)
+
+// console.log(output);
+
+
+
+
+const users = [
+    {firstName: "akshay", lastName: "saini", age:26},
+    {firstName: "donay", lastName: "juwail", age:75},
+    {firstName: "diwany", lastName: "renma", age:50},
+    {firstName: "akshay", lastName: "saini", age:26},
+]
+
+// const output = users.filter((person) => person.age>26)
+
+// console.log(output)
+
+
+// const secondoutput = users.reduce(function (accu,curr){
+//  if(accu[curr.age]){
+//     accu[curr.age] = ++accu[curr.age];
+//  }
+//  else{
+//     accu[curr.age] = 1;
+//  }return accu;
+// },{})
+
+// console.log(secondoutput)
+
+
+
+// const output = users.filter((person) => person.age<30).map((x) => x.firstName);
+// console.log(output)
+
+
+const array = [1,2,3,4];
+
+
+
+
+
+
